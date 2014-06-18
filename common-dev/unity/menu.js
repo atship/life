dcb926ebd8608aa27abfdc59491d4f1bb77020f4
Hualin Song <ijsonlin@gmail.com>
2014-06-18 17:48:49 +0800

    unity editor desc is added

--- common-dev/unity/menu.js
+++
@@ -1,8 +0,0 @@
-// Ctrl % Shift # Alt &
-// LEFT, RIGHT, UP, DOWN, F1 .. F12, HOME, END, PGUP, PGDN
-// This code will add a submenu under Tools, and the hotkey is Ctrl + Shift + E
-@MenuItem("Tools/This is test menu item %#E")
-static function Good()
-{
-	Debug.Log("Good item triggered");
-}

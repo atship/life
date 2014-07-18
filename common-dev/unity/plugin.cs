2c5f7efdcd3a222bd1c888dd1fb57b5ef0a81b64
Hualin Song <ijsonlin@gmail.com>
2014-07-18 10:29:31 +0800

    plugin bug fixed

--- common-dev/unity/plugin.cs
+++ common-dev/unity/plugin.cs
@@ -1,6 +1,6 @@
 1, first, create dynamically library, whatever mac/win/linux/android/ios, corresponding to platform arch, x86/x86_64/arm/...
 2, unitypro: put the binary file into the Assets/Plugins/ folder
-   unityfree: put c/c++ library into the project root folder, and c# library into the projectRoot/Plugins/ folder.
+   unityfree: put c/c++ library into the project root folder, and c# library into the Assets/Plugins/ folder.
    And after build for a target platform, copy the c/c++ library into the Data/Plugins/ folder
 3, create a c# wrapper class
 using System;
